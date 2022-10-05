ff1c0437f329ae62ed2445a54412460b163710da
kyrexa <arzamas.orko@yandex.ru>
2022-10-05 15:56:39 +0300

    Сделал домашнее задание семинар 2

---
+++ HomeWork_2/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string nStr = Convert.ToString(n);
+int n1 = nStr.Length;
+if (n1 > 2) 
+Console.WriteLine($"Третья цифра введённого числа: {nStr[2]}");
+else 
+Console.WriteLine("третьей цифры нет");
